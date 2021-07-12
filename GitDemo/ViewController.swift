88548c5fc3fd55872be8c84b288cf70dc47cdf52
Netanel <netanel77787@gmail.com>
2021-07-12 10:32:24 +0300

    improved gallery

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by נתנאל מנצור on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
